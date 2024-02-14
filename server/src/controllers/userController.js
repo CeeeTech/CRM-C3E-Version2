0e0d78fedda0a7a48a83f3da2fda03b76ef79090
CeeeTech <techceee@gmail.com>
2024-02-14 14:08:05 +0530

    user type change

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -218,7 +218,7 @@ async function updateUserByIdUsernameEmailUserTypeProductType(req, res) {
     const updateData = {
       name,
       email,
-      userType,
+      user_type: userType,
       product_type,
     };
 
