f6e81cf83f54903c18f254d07b5a7e23f3d56fe0
CeeeTech <techceee@gmail.com>
2024-03-13 11:23:25 +0530

    stash

--- server/src/controllers/leadController.js
+++ server/src/controllers/leadController.js
@@ -1495,6 +1495,68 @@ async function assignLeadsToCounselors() {
   }
 }
 
+async function sendVerificationCode(phoneNumber, verificationCode) {
+  const url = "https://richcommunication.dialog.lk/api/sms/send";
+  const apiKey = process.env.SMS_API_KEY;
+
+  // Normalize the phone number
+  const newPhoneNumber = normalizePhoneNumber(phoneNumber);
+
+  const data = {
+    messages: [
+      {
+        clientRef: "0934345",
+        number: newPhoneNumber,
+        mask: "SLTC", // Update the mask if necessary
+        text: `Your OTP code is ${verificationCode}. Please enter this code to complete registration.`,
+        campaignName: "rmstest",
+      },
+    ],
+  };
+
+  // Get the current date time in the required format
+  const currentDateTime = moment().format("YYYY-MM-DDTHH:mm:ss");
+  console.log("Current date time:", currentDateTime);
+
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        "Content-Type": "application/json",
+        "USER": "user_slt",
+        "DIGEST": "23383276670a8227dc53f93a952ccfa6",
+        "CREATED": currentDateTime,
+        "Authorization": `Bearer ${apiKey}`,
+      },
+    });
+
+    // Log the response status
+    console.log("Response status:", response.status);
+
+    // Check if response data exists
+    const responseData = response.data;
+    console.log("Response data:", responseData);
+
+    // Check if response status is OK
+    if (response.status === 200) {
+      console.log("Verification code sent successfully");
+      return true;
+    } else {
+      console.error("SMS sending failed:", responseData);
+      throw new Error(`SMS sending failed with status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error sending verification code:", error.message);
+    throw error;
+  }
+}
+
+function normalizePhoneNumber(phoneNumber) {
+  // Remove any non-digit characters
+  const digitsOnly = phoneNumber.replace(/\D/g, '');
+  // If the number starts with '0', remove it and prepend '94', else just return the number
+  return digitsOnly.startsWith('0') ? '94' + digitsOnly.slice(1) : digitsOnly;
+}
+
 async function assignLeadsToCounselorsTest(req, res) {
   const startTime = 8;
   const endTime = 17;
